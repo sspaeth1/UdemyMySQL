e897d91bfd7ec2ed7c07345e9f30ba87d0ba591b
sam.spaeth@gmail.com <sam.spaeth@gmail.com>
2019-12-04 19:34:37 -0600

    app update partial additions

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ var faker =  require('faker');
 for (var i = 0; i < 100 ; i++) {
    console.log(Number(ran));
 };
+
+
+faker.internet.email();
+faker.date.past();
+
+
+function generateAddress {
+  faker.address.streetAdress();
+  faker.address.city();
+  faker.address.state();
+};
