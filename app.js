ec3d3285da6f9015bc2f4bd83ca31285baacb1c2
sam.spaeth@gmail.com <sam.spaeth@gmail.com>
2019-12-04 22:25:04 -0600

    updated app.js with SQL require

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 var faker =  require('faker');
+var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'ig_clone'
+});
 
-for (var i = 0; i < 100 ; i++) {
-   console.log(Number(ran));
-};
+// for (var i = 0; i < 100 ; i++) {
+//    console.log(Number(ran));
+// };
+var generate = 'SELEC{T CURDATE()';
 
+//  function generateAddress(){
+//   faker.internet.email();
+//   faker.date.past();
+//   faker.address.streetAdress();
+//   faker.address.city();
+//   faker.address.state();
+// };
 
-faker.internet.email();
-faker.date.past();
+// generateAddress();
 
-
-function generateAddress {
-  faker.address.streetAdress();
-  faker.address.city();
-  faker.address.state();
-};
+connection.query(generate, function( error, results, fields){
+  if(error) throw error;
+  console.log('The random address is ', results);
+});
